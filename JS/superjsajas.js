c79c636a1fadb5b65e695d49991467f01239119e
pablomanez <pablomanez@hotmail.com>
2018-04-17 17:46:16 +0200

    Trabajando en nueva receta
    +Ya se pueden eliminar las fotos\n+Escrito el ejemplo que Juan pasó por whatsapp\n+:ok_hand:

--- JS/superjsajas.js
+++ JS/superjsajas.js
@@ -1,18 +1,21 @@
 /*
 TODO:
-	Solamente acceder estando logueado
-	Al añadir un nuevo ingrediente que se añada al final de la lista automaticamente		HECHO
-	Lo de añadir las fotos 																	HECHO (FALTA LO DE LA RUTA)
-	Tamaño de imagen máximo para las fotos (300kb) >> MENSAJE MODAL O EMERGENTE
-	Al enviar la receta si no hay ninguna foto, sedebe de avisar al usuario
+	NUEVARECETA:
+		Solamente acceder estando logueado
+		Al añadir un nuevo ingrediente que se añada al final de la lista automaticamente		HECHO
+		Lo de añadir las fotos 																	HECHO (FALTA LO DE LA RUTA)
+		Tamaño de imagen máximo para las fotos (300kb) >> MENSAJE MODAL O EMERGENTE
+		Al enviar la receta si no hay ninguna foto, sedebe de avisar al usuario
 
-	MÁS >> Punto c2
+		MÁS >> Punto c2
 
 
 	localhost/rest/get/receta/u=6      ULTIMAS 6 RECETAS
 */
 
 function ingrediente_masmas() {
+	//AÑADE UN INGREDIENTE MAS A LA LISTA DE NUEVA RECETA
+
 	let ingr = document.getElementById("new_ingredient").value; //EL INGREDIENTE
 	let list = document.getElementById("l_ingr"); 				//LA LISTA
 	let li = document.createElement("li");						//CREO EL LI
@@ -29,10 +32,10 @@ function ingrediente_masmas() {
 	//LOG
 	//console.log("No, Muzska no ha subido video :(");
 	//console.log("Aunque has añadido un/una "+ingr);
-	pruebaFetch();
 }
 
 function foto_masmas(){
+	//AÑADE UNA FOTO MAS A LA LISTA DE FOTOS DE UNA RECETA
 	let file = document.getElementById("input_foto").files[0].name;
 	let size = document.getElementById("input_foto").files[0].size;
 	
@@ -58,12 +61,21 @@ function foto_masmas(){
 		let img = document.createElement("img");
 		let textarea = document.createElement("textarea");
 
+		let j = 0;
+		while(true){
+			if(!document.getElementById("f_"+j)){
+				break;
+			}
+			j++;
+		}
+
 		//SPAN
 		let i = document.createElement("i");
 		i.setAttribute("class","fas fa-times");
 		span.appendChild(i);
 		span.setAttribute("class","position-absolute pointer t0 r0 m-2 text-blood h3 z-2");
-		span.setAttribute("onclick","borraFoto()");
+		span.setAttribute("id","b_"+j);
+		span.setAttribute("onclick","borraFoto(id)");
 
 		//IMG
 		//<img style="border-radius: 5px;" class="mr-2 Mw-100" src="Images/EL_LOGO.jpg" alt="Foto de la receta">
@@ -79,7 +91,7 @@ function foto_masmas(){
 		textarea.setAttribute("rows","5");
 		textarea.setAttribute("cols","35");
 		textarea.setAttribute("name","d_foto");
-		textarea.setAttribute("placeholder","Escribe una descripción`de la foto");
+		textarea.setAttribute("placeholder","Escribe una descripción de la foto");
 		textarea.setAttribute("maxlength","250");
 		textarea.setAttribute("required","true");
 
@@ -88,6 +100,7 @@ function foto_masmas(){
 		div.appendChild(img);
 		div.appendChild(textarea);
 		div.setAttribute("class","bg-dark-t box-shadow p-4 text-light mb-2 position-relative");
+		div.setAttribute("id","f_"+j)
 
 		list.appendChild(div);
 
@@ -97,14 +110,17 @@ function foto_masmas(){
 	}
 }
 
-function borraFoto(){
+function borraFoto(id){
+	//BORRAR UNA FOTO DE LA LISTA DE UNA RECETA
+	let n = id.substring(2);
+	
+	let div = document.getElementById("f_"+n);
+	console.log(div);
 
-
-
-
+	div.parentNode.removeChild(div);
 
 	//LOG
-	console.log("BORRAR");
+	//console.log("BORRAR");
 }
 
 
@@ -170,6 +186,35 @@ function hacerLogin(){
 	},function(response){
 
 	});
+
+}
+
+function hacerlogin2(frm){
+//EJEMPLO QUE PASÓ JUAN POR WHATSAPP
+	
+	let fd = new FormData();
+	let xhr = new XMLHttpRequest();
+	let url = '../rest/login';
+	let u, clave;
+
+	xhr.open('POST',url,true);
+	xhr.onload = function(){
+		console.log(xhr.responseText);
+		let r = JSON.parse(xhr.responseText);
+
+		if(r.RESPUESTA = 'OK'){
+			console.log(r);
+			sessionStorage.setItem('usuario',xhr.responseText);
+			u = JSON.parse(sessionStorage['usuario']);
+			clave = u.clave;
+		}
+		else{
+			console.log("EROEOROEROEROERO");
+		}
+
+	};
+
+
 
 }
 
