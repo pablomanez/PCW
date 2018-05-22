291558ae62f65df0c177ed213e90a128f376652e
pablomanez <pablomanez@hotmail.com>
2018-05-22 10:42:09 +0200

    Ya crea el array de deshordenacion

--- P3/canvas.js
+++ P3/canvas.js
@@ -4,6 +4,13 @@ const _ALTO = 240;
 var ncols;
 var nrows;
 
+var imgCargada = false;
+
+// PIEZAS ALMACENA LA POSICION Y LAS DIMENSIONES
+// DE TODOS LOS CUADROS DEL PUZZLE
+// PARA DESPUES INVOCAR EL getImageData(...) 
+var piezas = [];
+
 function getCTX(query){
 	let cv = document.querySelector(query);
 
@@ -42,7 +49,8 @@ function prepararCanvases(){
 			img.onload = function(){
 				let ctx = c1.getContext('2d');
 				ctx.drawImage(img,0,0,c1.width,c1.height);
-				
+				imgCargada = true;
+
 				copiarCanvas();	//CARGA DE FORMA ASÍNCRONA ÓPTIMA :ok_hand:
 			};
 			img.src = fr.result;
@@ -51,67 +59,73 @@ function prepararCanvases(){
 	};
 }
 
+//FUNCION PRINCIPAL
 function copiarCanvas(){
-	let query = '#c1';
-		let cv1 = getCV(query);
-		let ctx1 = getCTX(query);
+	if(imgCargada){
+		let query = '#c1';
+			let cv1 = getCV(query);
+			let ctx1 = getCTX(query);
 
-	query = '#c2';
-		let cv2 = getCV(query);
-		let ctx2 = getCTX(query);
+		query = '#c2';
+			let cv2 = getCV(query);
+			let ctx2 = getCTX(query);
 
 
-	let imgData = ctx1.getImageData(0,0,cv1.width,cv1.height);
+		let imgData = ctx1.getImageData(0,0,cv1.width,cv1.height);
 
-	ctx2.putImageData(imgData,0,0);
+		ctx2.putImageData(imgData,0,0);
 
-	//CREO LAS LINEAS
-	dibujarLineas();
+		//CREO LAS LINEAS
+		dibujarLineas();
+
+		creaArrayPiezas();
+	}
 }
 
 function dibujarLineas(){
-	let query = '#c2';
-	let cv = getCV(query);
-	let ctx = getCTX(query);
+	if(imgCargada){
+		let query = '#c2';
+		let cv = getCV(query);
+		let ctx = getCTX(query);
 
-	let diff = document.querySelector("#diff");
-	if(diff.value == 0){
-		ncols = 4;
-		nrows = 6;
-	}
-	else if(diff.value == 1){
-		ncols = 6;
-		nrows = 9;	
-	}
-	else if(diff.value == 2){
-		ncols = 8;
-		nrows = 12;
-	}
-	else{
-		ncols = 4;
-		nrows = 3;
-	}
+		let diff = document.querySelector("#diff");
+		if(diff.value == 0){
+			ncols = 4;
+			nrows = 6;
+		}
+		else if(diff.value == 1){
+			ncols = 6;
+			nrows = 9;	
+		}
+		else if(diff.value == 2){
+			ncols = 8;
+			nrows = 12;
+		}
+		else{
+			ncols = 4;
+			nrows = 3;
+		}
 
-	let dimy = cv.width/nrows;
-	let dimx = cv.height/ncols;
+		let dimy = cv.width/nrows;
+		let dimx = cv.height/ncols;
 
-	ctx.beginPath();
-	ctx.strokeStyle = document.querySelector("#color").value;
-	ctx.lineWidth = 5;
+		ctx.beginPath();
+		ctx.strokeStyle = document.querySelector("#color").value;
+		ctx.lineWidth = 5;
 
-	for(let i=0 ; i<ncols ; i++){
-		ctx.moveTo(0, i*dimx);
-		ctx.lineTo(cv.width, i*dimx);
+		for(let i=0 ; i<ncols ; i++){
+			ctx.moveTo(0, i*dimx);
+			ctx.lineTo(cv.width, i*dimx);
+		}
+		
+		for(let i=0 ; i<nrows ; i++){
+			ctx.moveTo(i*dimy, 0);
+			ctx.lineTo(i*dimy, cv.height);
+		}
 
+		ctx.rect(0,0,cv.width,cv.height);
+		ctx.stroke();
 	}
-	
-	for(let i=0 ; i<nrows ; i++){
-		ctx.moveTo(i*dimy, 0);
-		ctx.lineTo(i*dimy, cv.height);
-	}
-
-	ctx.rect(0,0,cv.width,cv.height);
-	ctx.stroke();
 }
 
 function cargaImg(input){
@@ -124,12 +138,88 @@ function cargaImg(input){
 	img.onload = function(){
 		//PROBAR CON LOCALHOST
 		ctx.drawImage(img,0,0,c1.width,c1.height);
+
+		imgCargada = true;
+
 		copiarCanvas();
 	};
 	img.src = URL.createObjectURL(input.files[0]);
 
 	//console.log(input.files[0]);
 }
+
+//SE LLAMA DESDE COPIARCANVAS
+function creaArrayPiezas(){
+	let query = '#c1';
+		let cv1 = getCV(query);
+		let ctx1 = getCTX(query);
+	
+	query = '#c2';
+		let cv2 = getCV(query);
+		let ctx2 = getCTX(query);
+
+	piezas = [];
+	let imgData;
+
+	let x = cv1.width/nrows;
+	let y = cv1.height/ncols;
+
+	//FILAS > COLUMNAS
+	for(let i=0 ; i<ncols ; i++){
+		for(let j=0 ; j<nrows ; j++){
+			//imgData = ctx1.getImageData(x*j,y*i,x,y);
+			imgData = [x*j,y*i,x,y];
+
+			piezas.push(imgData);
+		}
+	}
+
+	//console.log(piezas);
+}
+
+function comiensaElPusle(){
+	if(imgCargada){
+		console.log("Se ha cargado una imagen y puedes empezar");
+
+		deshordena();
+	}
+	else{
+		console.log("Como no cargues una imagen, golpe de remo");
+	}
+}
+
+function deshordena(){
+	let min = 0;
+	let max = (ncols*nrows)-1;
+
+	let aux = -1;
+	let aux2 = -1;
+	for(let i=0 ; i<=max ; i++){
+		aux = getRandom(min,max);
+
+		while(aux2==-1 || aux2==aux){
+			aux2 = getRandom(min,max);
+		}
+
+		//AUX Y AUX2 SIEMPRE VAN A SER DISTINTOS
+		let p_aux = piezas[aux];
+		piezas[aux] = piezas[aux2];
+		piezas[aux2] = p_aux;
+
+
+
+		aux = -1;
+		aux2 = -1;
+	}
+
+	console.log(piezas);
+}
+
+//DEVUELVE UN NUMERO RANDOM ENTRE min Y max INCLUIDOS
+function getRandom(min,max){
+	return Math.floor((Math.random() * (max+1)) + min);
+}
+
 
 //<TRON>
 /*
